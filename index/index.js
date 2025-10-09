f053b7351934b68a190feca9d345d9b4415ef9c6
Jesusale1234 <jesusrayado632@gmail.com>
2025-10-09 17:13:23 -0600

    Merge branch 'main' of https://github.com/Jesusale1234/RESTfulAPI

--- index/index.js
+++ index/index.js
@@ -5,6 +5,7 @@ import routes from './routes/routes.js';
 
 const mongoString = process.env.DATABASE_URL;
 
+// Function to connect to the MongoDB database.
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
@@ -12,19 +13,23 @@ database.on('error', (error) => {
     console.log(error)
 })
 
+// It makes the connection to the database, 
+// then provides a message in the log mentioning 'Database Connected'.
 database.once('connected', () => {
     console.log('Database Connected');
 })
 const app = express();
 
+// It initializes the HTTP parameters of the API.
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('API funcionando correctamente 🚀');
+  res.send('API Working! 🚀');
 });
 
+// It relates the routes in routes.js as a group called "/api".
 app.use('/api', routes);
-console.log("Rutas /api registradas correctamente ✅");
+console.log("/api routes loaded ✅");
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
--- model.js
+++ model.js
@@ -1,19 +1,27 @@
 import mongoose from 'mongoose';
 
-
+// To define the database elements that will be considered
 const restaurantSchema = new mongoose.Schema({
+  
+  // Name of the restaurant.
   name: {
     type: String,
     required: true
   },
+
+  // Type of cuisine.
   category: {
-    type: String, // tipo de comida, ej. "Italiana", "Mexicana"
+    type: String,
     required: true
   },
+
+  // Location, City.
   location: {
     type: String,
     required: true
   },
+
+  // Rating in a scale from 0 to 5, depending on the customers' evaluation.
   rating: {
     type: Number,
     min: 0,
@@ -22,4 +30,5 @@ const restaurantSchema = new mongoose.Schema({
   }
 });
 
+// To export the complete structure to other classes, being references as 'Restaurant'.
 export default mongoose.model('Restaurant', restaurantSchema);
